ff610396c12450992efa780aaa9aa0c72226b27d
Lorenzo C. Bianchi <bianchi92@hotmail.com>
2021-11-23 00:18:09 -0300

    Add rota criar e o arquivo criar.ejs

--- index.js
+++ index.js
@@ -7,12 +7,10 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded());
 
-
 require("dotenv").config();
 const Filme = require("./models/filme"); // Faz a conexão com o banco e recebe o modelo do Sequelize
 
 const message = "";
-
 
 app.get("/", async (req, res) => {
   const filmes = await Filme.findAll();
@@ -22,9 +20,27 @@ app.get("/", async (req, res) => {
 // Rota para trazer os detalhes de um filme através do seu ID(PK)
 app.get("/filmes/:id", async (req, res) => {
   const filme = await Filme.findByPk(req.params.id);
-  res.render("detalhes", {
-    filme,
+  res.render("detalhes", { filme });
+});
+
+app.get("/criar", (req, res) => {
+  res.render("criar", { message });
+});
+
+// Rota 'Post' chamada quando o formulario for submetido - (submit no criar.ejs)
+app.post("/criar", async (req, res) => {
+  const { nome, imagem, descricao } = req.body;
+
+  // Método create
+  const filme = await Filme.create({
+    nome,
+    imagem,
+    descricao,
   });
+
+  message = "Novo filme adicionado!";
+
+  res.render("criar", { message });
 });
 
 app.listen(port, () =>
